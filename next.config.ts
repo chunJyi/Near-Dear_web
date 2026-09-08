32738c4aafefb43b704ace8be4e77b7c727de9b3
chunJyi <chunjyicj@gmail.com>
2026-09-08 23:52:36 +0630

    fix not found resouces

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Near-Dear_web',
   images: {
     unoptimized: true,
   },
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -182,7 +182,7 @@ export function Hero() {
             <PhoneMockup color="black" className="relative z-10">
               <div className="w-full h-full">
                 <img
-                  src="/Near-Dear_web/images/home.jpg"
+                  src="/images/home.jpg"
                   alt="Near&Dear App Screenshot home"
                   className="w-full h-full object-cover"
                 />
